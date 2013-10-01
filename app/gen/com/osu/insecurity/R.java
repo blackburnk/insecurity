2e65cd04803acca858c9ccbd3d0861b6ffa79dd4
Kathryn Blackburn <blackburn.171@buckeyemail.osu.edu>
2013-10-01 19:09:15 -0400

    Camerons stuff with a few updates from Kathy

--- app/gen/com/osu/insecurity/R.java
+++ app/gen/com/osu/insecurity/R.java
@@ -23,19 +23,57 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int security_icon=0x7f020001;
+        public static final int security_icon_2=0x7f020002;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
-        public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070001;
+        public static final int forgotPassword_back_text=0x7f070003;
+        public static final int forgotPassword_email_address_edit_text=0x7f070001;
+        public static final int forgotPassword_email_address_text=0x7f070000;
+        public static final int forgotPassword_send_password_button=0x7f070002;
+        public static final int fullscreen_content=0x7f070004;
+        public static final int fullscreen_content_controls=0x7f07000c;
+        public static final int main_email_address_edit_text=0x7f070006;
+        public static final int main_email_address_text=0x7f070005;
+        public static final int main_forgot_password_text=0x7f07000b;
+        public static final int main_login_button=0x7f070009;
+        public static final int main_password_edit_text=0x7f070008;
+        public static final int main_password_text=0x7f070007;
+        public static final int main_register_text=0x7f07000a;
+        public static final int register_back_text=0x7f070016;
+        public static final int register_confirm_email_address_edit_text=0x7f070010;
+        public static final int register_confirm_email_address_text=0x7f07000f;
+        public static final int register_confirm_password_edit_text=0x7f070014;
+        public static final int register_confirm_password_text=0x7f070013;
+        public static final int register_email_address_edit_text=0x7f07000e;
+        public static final int register_email_address_text=0x7f07000d;
+        public static final int register_password_edit_text=0x7f070012;
+        public static final int register_password_text=0x7f070011;
+        public static final int register_register_button=0x7f070015;
     }
     public static final class layout {
-        public static final int login_layout=0x7f030000;
+        public static final int forgot_password_layout=0x7f030000;
+        public static final int login_layout=0x7f030001;
+        public static final int register_layout=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int dummy_button=0x7f050001;
-        public static final int dummy_content=0x7f050002;
+        public static final int forgotPassword_back=0x7f050010;
+        public static final int forgotPassword_emailAddress=0x7f05000e;
+        public static final int forgotPassword_sendPasswordButton=0x7f05000f;
+        public static final int main_emailAddress=0x7f050003;
+        public static final int main_forgotPassword=0x7f050006;
+        public static final int main_login=0x7f050002;
+        public static final int main_loginButton=0x7f050007;
+        public static final int main_password=0x7f050004;
+        public static final int main_register=0x7f050005;
+        public static final int register_back=0x7f05000d;
+        public static final int register_confirmEmailAddress=0x7f05000a;
+        public static final int register_confirmPassword=0x7f05000c;
+        public static final int register_emailAddress=0x7f050009;
+        public static final int register_password=0x7f05000b;
+        public static final int register_registerButton=0x7f050008;
     }
     public static final class style {
         /** 
--- app/src/com/osu/insecurity/Login.java
+++ app/src/com/osu/insecurity/Login.java
@@ -1,14 +1,19 @@
 package com.osu.insecurity;
 
-import com.osu.insecurity.util.SystemUiHider;
-
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.osu.insecurity.util.SystemUiHider;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -44,16 +49,141 @@ public class Login extends Activity {
 	 * The instance of the {@link SystemUiHider} for this activity.
 	 */
 	private SystemUiHider mSystemUiHider;
-
+	
+	/**
+	 * The incorrect email address/password Toast string
+	 */
+	private static final String LOGIN_INCORRECT = "Incorrect email address/password";
+	
+	/**
+	 * The email address already taken Toast string
+	 */
+	private static final String REGISTER_TAKEN_EMAIL_ADDRESS = "This email address is already taken";
+	
+	/**
+	 * The successful registration Toast string
+	 */
+	private static final String REGISTER_REGISTRATION_SUCCESSFUL = "Registration successful";
+	
+	/**
+	 * The invalid registration Toast string
+	 */
+	private static final String REGISTER_REGISTRATION_INVALID = "Invalid registration/email";
+	
+	/**
+	 * The successful password sent Toast string
+	 */
+	private static final String FORGOT_PASSWORD_EMAIL_SENT = "Password sent to email";
+	
+	/**
+	 * Regular Expression Email Pattern
+	 */
+	private static final String EMAIL_PATTERN = 
+			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	/**
+	 * Below are the controls of the Login menu
+	 * login_emailAddressEditText = email address edit text box
+	 * login_passwordEditText = password edit text box
+	 * login_registerText = register text view
+	 * login_forgotPasswordText = forgot password text view
+	 * login_loginButton = login button
+	 */
+	private EditText login_emailAddressEditText;
+	private EditText login_passwordEditText;
+	private TextView login_registerText;
+	private TextView login_forgotPasswordText;
+	private Button login_loginButton;
+	
+	/**
+	 * Below are the controls for the register menu
+	 * register_registerButton = register button
+	 * register_backText = back text view
+	 * register_emailAddressEditText = email address edit text
+	 * register_confirmEmailAddressEditText = confirm email address edit text
+	 * register_passwordEditText = password edit text
+	 * register_confirmPasswordEditText = confirm password edit text
+	 */
+	private Button register_registerButton;
+	private TextView register_backText;
+	private EditText register_emailAddressEditText;
+	private EditText register_confirmEmailAddressEditText;
+	private EditText register_passwordEditText;
+	private EditText register_confirmPasswordEditText;
+	
+	/**
+	 * Below are the controls for the forgot password menu
+	 * forgotPassword_emailAddressEditText = email address edit text
+	 * forgotPassword_sendPassword = send password button
+	 * forgotPassword_backText = back text view
+	 */
+	private EditText forgotPassword_emailAddressEditText;
+	private Button forgotPassword_sendPassword;
+	private TextView forgotPassword_backText;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		login();
+	}
 
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		// Trigger the initial hide() shortly after the activity has been
+		// created, to briefly hint to the user that UI controls
+		// are available.
+		delayedHide(100);
+	}
+	
+	/**
+	 * Touch listener to use for in-layout UI controls to delay hiding the
+	 * system UI. This is to prevent the jarring behavior of controls going away
+	 * while interacting with activity UI.
+	 */
+	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if (AUTO_HIDE) {
+				delayedHide(AUTO_HIDE_DELAY_MILLIS);
+			}
+			return false;
+		}
+	};
+
+	Handler mHideHandler = new Handler();
+	Runnable mHideRunnable = new Runnable() {
+		@Override
+		public void run() {
+			mSystemUiHider.hide();
+		}
+	};
+
+	/**
+	 * Schedules a call to hide() in [delay] milliseconds, canceling any
+	 * previously scheduled calls.
+	 */
+	private void delayedHide(int delayMillis) {
+		mHideHandler.removeCallbacks(mHideRunnable);
+		mHideHandler.postDelayed(mHideRunnable, delayMillis);
+	}
+	
+	/**
+	 * Sets up the login screen
+	 */
+	private void login()
+	{
 		setContentView(R.layout.login_layout);
 
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.fullscreen_content);
-
+		login_emailAddressEditText = (EditText) findViewById(R.id.main_email_address_edit_text);
+		login_passwordEditText = (EditText) findViewById(R.id.main_password_edit_text);
+		login_registerText = (TextView) findViewById(R.id.main_register_text);
+		login_forgotPasswordText = (TextView) findViewById(R.id.main_forgot_password_text);
+		login_loginButton = (Button) findViewById(R.id.main_login_button);
+		
 		// Set up an instance of SystemUiHider to control the system UI for
 		// this activity.
 		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
@@ -114,49 +244,256 @@ public class Login extends Activity {
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
 		// while interacting with the UI.
-		findViewById(R.id.dummy_button).setOnTouchListener(
-				mDelayHideTouchListener);
+		
+		//sets up the interaction when the user clicks the login button
+		login_loginButton.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				if(loginSuccessful()) //check to see if the user entered correct email address/password
+				{
+					//go to the main screen
+				}
+				else
+				{
+					Toast.makeText(getApplicationContext(), LOGIN_INCORRECT, Toast.LENGTH_SHORT).show();
+				}
+			}	
+		});
+		
+		//sets up the interaction when the user clicks the register text
+		login_registerText.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				register();
+			}	
+		});
+		
+		//sets up the interaction when the user clicks the forgot password text
+		login_forgotPasswordText.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				forgotPassword();
+			}	
+		});
 	}
-
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		// Trigger the initial hide() shortly after the activity has been
-		// created, to briefly hint to the user that UI controls
-		// are available.
-		delayedHide(100);
-	}
-
 	/**
-	 * Touch listener to use for in-layout UI controls to delay hiding the
-	 * system UI. This is to prevent the jarring behavior of controls going away
-	 * while interacting with activity UI.
+	 * 
+	 * @return true if the email address/password is correct, otherwise return false
 	 */
-	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-			if (AUTO_HIDE) {
-				delayedHide(AUTO_HIDE_DELAY_MILLIS);
+	private boolean loginSuccessful()
+	{
+		boolean success = true; //TODO change to false when we establish database/server
+		if(login_emailAddressEditText.getText().toString().length() != 0) //check to see if the user has entered an email address
+		{
+			if(login_passwordEditText.getText().toString().length() != 0 ) //check to see if the user has entered a password
+			{
+				//check the email address/password with the data base
 			}
-			return false;
 		}
-	};
-
-	Handler mHideHandler = new Handler();
-	Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			mSystemUiHider.hide();
-		}
-	};
-
-	/**
-	 * Schedules a call to hide() in [delay] milliseconds, canceling any
-	 * previously scheduled calls.
-	 */
-	private void delayedHide(int delayMillis) {
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
+		return success;
 	}
+	
+	/**
+	 * 
+	 * @return true if the registration is successful, otherwise return false
+	 */
+	private boolean registrationSuccessful()
+	{
+		boolean success = false;
+				
+		if(register_emailAddressEditText.getText().toString().length() != 0) //check to see if the user has entered an email address
+		{
+			if(register_confirmEmailAddressEditText.getText().toString().length() != 0 ) //check to see if the user has entered a confirmed email
+			{
+				if(register_passwordEditText.getText().toString().length() != 0) //check to see if the user has entered a password
+				{
+					if(register_confirmPasswordEditText.getText().toString().length() != 0 ) //check to see if the user has entered a confirmed password
+					{
+						if(register_emailAddressEditText.getText().toString().equals(register_confirmEmailAddressEditText.getText().toString()))
+						{
+							if(register_passwordEditText.getText().toString().equals(register_confirmPasswordEditText.getText().toString()))
+							{
+								if(register_emailAddressEditText.getText().toString().matches(EMAIL_PATTERN))
+								{
+									//check database for taken email address or register 
+									/*
+									if(!database.contains(register_emailAddressEditText.getText().toString())
+									{
+										//register the email
+										success = true;
+									}
+									else
+									{
+										Toast.makeText(getApplicationContext(), REGISTER_TAKEN_EMAIL_ADDRESS, Toast.LENGTH_SHORT).show();
+									}
+									*/
+								}
+								else //have not entered a valid email address
+								{
+									Toast.makeText(getApplicationContext(), REGISTER_REGISTRATION_INVALID, Toast.LENGTH_SHORT).show();
+								}
+							}
+							else //passwords do not match
+							{
+								Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
+							}
+						}
+						else //email addresses do not match
+						{
+							Toast.makeText(getApplicationContext(), "Email addresses do not match", Toast.LENGTH_SHORT).show();
+						}
+					}
+					else //didn't enter a confirmed password
+					{
+						Toast.makeText(getApplicationContext(), "Please enter password", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else //didn't enter a password
+				{
+					Toast.makeText(getApplicationContext(), "Please enter password", Toast.LENGTH_SHORT).show();
+				}
+			}
+			else //didn't enter a confirmed email
+			{
+				Toast.makeText(getApplicationContext(), "Please enter an email address", Toast.LENGTH_SHORT).show();
+			}
+		}
+		else //didn't enter an email
+		{
+			Toast.makeText(getApplicationContext(), "Please enter an email address", Toast.LENGTH_SHORT).show();
+		}
+		return success;
+	}
+	
+	/**
+	 * Changes the screen to the register registration page
+	 */
+	private void register()
+	{
+		setContentView(R.layout.register_layout);
+		
+		register_registerButton = (Button) findViewById(R.id.register_register_button);
+		register_backText = (TextView) findViewById(R.id.register_back_text);
+		register_emailAddressEditText = (EditText) findViewById(R.id.register_email_address_edit_text);
+		register_confirmEmailAddressEditText = (EditText) findViewById(R.id.register_confirm_email_address_edit_text);;
+		register_passwordEditText = (EditText) findViewById(R.id.register_password_edit_text);
+		register_confirmPasswordEditText = (EditText) findViewById(R.id.register_confirm_password_edit_text);
+		
+		//sets up the interaction when the user clicks the register button
+		register_registerButton.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				if(registrationSuccessful())
+				{
+					Toast.makeText(getApplicationContext(), REGISTER_REGISTRATION_SUCCESSFUL, Toast.LENGTH_SHORT).show();
+				}
+			}	
+		});
+		
+		//sets up the interaction when the user clicks the back text
+		register_backText.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				//go back to the login menu
+				login();
+			}	
+		});
+	}
+	
+	/**
+	 * 
+	 * @return true if the password was sent, otherwise return false
+	 */
+	private boolean passwordSent()
+	{
+		boolean success = false;
+		
+		if(forgotPassword_emailAddressEditText.getText().toString().matches(EMAIL_PATTERN))
+		{
+			//send the password to that email
+			success = true;
+		}
+		else
+		{
+			Toast.makeText(getApplicationContext(), REGISTER_REGISTRATION_INVALID, Toast.LENGTH_SHORT).show();
+		}
+
+		return success;
+	}
+	
+	/**
+	 * Changes the screen to the forgot password screen
+	 */
+	private void forgotPassword()
+	{
+		setContentView(R.layout.forgot_password_layout);
+		
+		forgotPassword_emailAddressEditText = (EditText) findViewById(R.id.forgotPassword_email_address_edit_text);
+		forgotPassword_sendPassword = (Button) findViewById(R.id.forgotPassword_send_password_button);
+		forgotPassword_backText = (TextView) findViewById(R.id.forgotPassword_back_text);
+		
+		//sets up the interaction when the user clicks the register button
+		forgotPassword_sendPassword.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				if(passwordSent())
+				{
+					Toast.makeText(getApplicationContext(), FORGOT_PASSWORD_EMAIL_SENT, Toast.LENGTH_SHORT).show();
+				}
+			}	
+		});
+				
+				//sets up the interaction when the user clicks the back text
+		forgotPassword_backText.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				//go back to the login menu
+				login();
+			}	
+		});
+	}
+	
+	/**
+	 * Creates the options menu and the bottom of the application.
+	 */
+	public boolean onCreateOptionsMenu(Menu menu)
+ 	{
+ 		super.onCreateOptionsMenu(menu);
+ 		//add an option to the menu and sets the properties
+ 		menu.add( 0,1, 1, "Exit Insecurity" )
+        .setShortcut('1', 'e')
+        .setIcon(android.R.drawable.ic_menu_close_clear_cancel);
+ 		return true;
+ 	}
+	
+	/**
+	 * Handles the events when the options menu is selected and when the options on the menu are selected
+	 */
+ 	public boolean onOptionsItemSelected(MenuItem item)
+ 	{
+ 		super.onOptionsItemSelected(item);
+ 		switch (item.getItemId()){
+ 		case 1:
+ 			//exits the application and kills all processes
+ 			finish();
+ 			android.os.Process.killProcess(android.os.Process.myPid());
+ 			return true;
+
+ 		}
+ 		return super.onOptionsItemSelected(item);
+ 	}
 }
